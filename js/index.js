a517a32b6561f9f84e9825b13173bacc9399a6e3
Maria <konstantinovamaria25@gmail.com>
2022-11-01 20:42:37 +0300

    Generate 4 operants.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-import BigNumber from "../node_modules/bignumber.js/bignumber.mjs";
 
 
 const prettierValue = (val) => {
@@ -6,7 +5,6 @@ const prettierValue = (val) => {
     const re1 = /\s/gi;
     let res = val.replace(re, '.');
     res = res.replace(re1,'');
-    console.log(res)
     return res;
 }
 
@@ -16,62 +14,141 @@ const checkValue = (val) => {
 }
 
 const makeSixSignsAfterComma = (bigNumberObj) => {
-    console.log(bigNumberObj);
     const numberOfDigits = bigNumberObj.c[0].toString().length + 6;
     return bigNumberObj.precision(numberOfDigits);
 }
 
-
 const prerrierResult = (bigNumObj) => {
-    return bigNumObj.toString().replace(/(\d)(?=(\d\d\d)+([^\d]))/g, '$1 ');
+    const res = makeSixSignsAfterComma(bigNumObj);
+    return res.toString().replace(/(\d)(?=(\d\d\d)+([^\d]))/g, '$1 ');
 }
 
-const operator1 = document.getElementById('input_operator1');
-const operator2 = document.getElementById('input_operator2');
+
+const checkValues = (arr) =>{
+    return arr.filter((item)=> !checkValue(item));
+}
+
+const getPriority = (action1, action2) =>{
+    const resArr = [null,null]
+    switch (action1.value){
+        case '+':
+            resArr[0] = 0;
+            break;
+        case '-':
+            resArr[0] = 0;
+            break;
+        case '*':
+            resArr[0] = 1;
+            break;
+        case '/':
+            resArr[0] = 1;
+            break;
+    }
+
+    switch (action2.value){
+        case '+':
+            resArr[1] = 0;
+            break;
+        case '-':
+            resArr[1] = 0;
+            break;
+        case '*':
+            resArr[1] = 1;
+            break;
+        case '/':
+            resArr[1] = 1;
+            break;
+    }
+
+    return resArr[0] >= resArr[1];
+}
+
+
+const performOperation = (actionValue, f, s) => {
+    let res;
+    switch (actionValue){
+        case '+':
+            res = f.plus(s);
+            break;
+        case '-':
+            res = f.minus(s);
+            break;
+        case '*':
+            res = f.multipliedBy(s);
+            break;
+        case '/':
+            if(s == 0){
+                alert('Division by zero :3');
+                break;
+            }
+            res = f.dividedBy(s);
+            break;
+        default: break;
+    }
+
+    console.log(res);
+    return res;
+}
+
+
+const value1 = document.getElementById('input_operator1');
+const value2 = document.getElementById('input_operator2');
+const value3 = document.getElementById('input_operator3');
+const value4 = document.getElementById('input_operator4');
 const result = document.getElementById('input_result');
 const buttonGetRes = document.getElementById('get_result');
 
-const operators = document.querySelectorAll('input[name="operator"]')
+const operators1 = document.querySelectorAll('input[name="operator1"]')
+const operators2 = document.querySelectorAll('input[name="operator2"]')
+const operators3 = document.querySelectorAll('input[name="operator3"]')
 
 
 buttonGetRes.addEventListener('click', (ev) => {
     ev.preventDefault();
 
-    if(!checkValue(operator1.value)){
-        alert(`"${operator1.value}" its an error value`);
-    }else if(!checkValue(operator2.value)){
-        alert(`"${operator2.value}" its an error value`);
-    }else {
-        const a = new BigNumber(prettierValue(operator1.value));
-        const b = new BigNumber(prettierValue(operator2.value));
+    const check = checkValues([value1.value, value2.value, value3.value, value4.value]);
 
-        let action;
-        for (const operator of operators) {
+    if(check.length){
+        check.forEach((item)=>alert(`"${item}" its an error value`))
+    }else {
+        const a = new BigNumber(prettierValue(value1.value));
+        const b = new BigNumber(prettierValue(value2.value));
+        const c = new BigNumber(prettierValue(value3.value));
+        const d = new BigNumber(prettierValue(value4.value));
+
+        let innerResult;
+
+        let action1;
+        for (const operator of operators1) {
             if (operator.checked) {
-                action = operator;
+                action1 = operator;
+            }
+        }
+        let action2;
+        for (const operator of operators2) {
+            if (operator.checked) {
+                action2 = operator;
+            }
+        }
+        let action3;
+        for (const operator of operators3) {
+            if (operator.checked) {
+                action3 = operator;
             }
         }
 
-        switch (action.value){
-            case '+':
-                result.value = prerrierResult(a.plus(b));
-                break;
-            case '-':
-                result.value = prerrierResult(a.minus(b));
-                break;
-            case '*':
-                result.value = prerrierResult(makeSixSignsAfterComma(a.multipliedBy(b)));
-                break;
-            case '/':
-                if(b == 0){
-                    alert('Division by zero :3');
-                    break;
-                }
-                result.value =  prerrierResult(makeSixSignsAfterComma(a.dividedBy(b)));
-                break;
-            default: break;
-        }
 
+        innerResult = performOperation(action2.value, b, c);
+        console.log('1',innerResult)
+
+        if(getPriority(action1, action3)){
+            innerResult = performOperation(action1.value, a, innerResult);
+            result.value = prerrierResult(performOperation(action3.value, innerResult, d));
+        }
+        else {
+            innerResult = performOperation(action3.value, innerResult, d);
+            result.value = prerrierResult(performOperation(action1.value, a, innerResult));
+        }
 
     }
 })
